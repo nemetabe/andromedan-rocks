a68a691dc70abbc32fb38abd009f93a698d5367a
nemetabe <153465162+nemetabe@users.noreply.github.com>
2025-12-28 22:25:36 +0100

    chore(core-loop): add core game loop with input and movement systems and event bus, called respectfully to time

---
+++ game/core/game.py
@@ -0,0 +1,20 @@
+class Game:
+	def __init__(self, time, input_system, movement_system, world, event_bus):
+		self.time = time
+		self.input = input_system
+		self.movement = movement_system
+		self.event_bus = event_bus
+		self.world = world
+		self.running = True
+
+	def run(self):
+		self.time.reset()
+
+		while self.running:
+			dt = self.time.tick()
+
+			self.input.update()
+			self.event_bus.flush()
+
+			self.movement.update(dt)
+			self.event_bus.flush()

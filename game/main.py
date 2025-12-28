5652894e8375387ea22ad4796743097bfd8c1a3f
nemetabe <153465162+nemetabe@users.noreply.github.com>
2025-12-28 22:21:29 +0100

    chore(core-loop): game is initialised, with dependencies wired in

--- game/main.py
+++ game/main.py
@@ -1,6 +1,33 @@
-def main():
-    pass
+from clock import Clock
+from core import Game, EventBus, World
+from entities import SpaceShip
+from systems import InputSystem, MovementSystem
+
+
+class Main:
+	def __init__(self):
+		pass
+
+	def run():
+		event_bus = EventBus()
+
+		space_ship = SpaceShip(event_bus)
+		world = World([space_ship])
+		
+		input_sytem = InputSystem(event_bus)
+		movement_system = MovementSystem(world)
+
+		game = Game(
+			time=Clock(),
+			input=input_sytem,
+			movement=movement_system,
+			world=world,
+			event_bus=event_bus
+		)
+
+		game.run()
 
 
 if __name__ == "__main__":
-    main()
+	main = Main()
+	main.run()
